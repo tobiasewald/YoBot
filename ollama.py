e38e5c733e20a4aa9c584dc06a1676592dbc88c6
Eiscreme <eiscreme@Mac.fritz.box>
2025-04-05 20:40:06 +0200

    add ollama

--- ollama.py
+++ ollama.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_ollama_suggestions(log_data):
-    url = "http://localhost:8080/analyze-log"
+    url = "http://ollama-service:8081/analyze-log"  # Der Kubernetes-Service-Name
     response = requests.post(url, json={"log": log_data})
     if response.status_code == 200:
         return response.json()["suggestions"]
